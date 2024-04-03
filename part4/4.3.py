a1041c9417ba57e14685a6d99b63c0e651d3a4ea
Nemenko Andrey <an.nemenko@yandex.ru>
2024-04-03 18:33:47 +0300

    part 4 final

--- part4/4.3.py
+++ part4/4.3.py
@@ -1,9 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
 from tensorflow import keras
 from tensorflow.keras import layers
 
 from part4.model_wrapper import ModelWrapper
 
-(train_data, train_labels), (test_data, test_labels) = keras.datasets.boston_housing.load_data()
+(train_data, train_targets), (test_data, test_targets) = keras.datasets.boston_housing.load_data()
 
 mean = train_data.mean(axis=0)
 train_data -= mean
@@ -24,3 +26,33 @@ def build_model():
 k = 4
 
 num_val_samples = len(train_data) // k
+num_epoch = 100
+all_scores = []
+all_history = []
+
+for i in range(k):
+    print(f"Progressing fold #{i}")
+    val_data = train_data[i * num_val_samples: (i + 1) * num_val_samples]
+    val_targets = train_targets[i * num_val_samples: (i + 1) * num_val_samples]
+
+    partial_train_data = np.concatenate([train_data[:i * num_val_samples],
+                                         train_data[(i + 1) * num_val_samples:]], axis=0)
+    partial_train_targets = np.concatenate([train_targets[:i * num_val_samples],
+                                            train_targets[(i + 1) * num_val_samples:]], axis=0)
+
+    model = build_model()
+    history = model.fit(x=partial_train_data, y=partial_train_targets, epochs=num_epoch, batch_size=16, verbose=0)
+    val_mse, val_mae = model.evaluate(val_data, val_targets, verbose=1)
+    all_scores.append(val_mae)
+
+    all_history.append(history.history["mae"])
+
+print(all_scores)
+print(np.mean(all_scores))
+
+average_history = [np.mean([x[i] for x in all_history]) for i in range(num_epoch)]
+
+plt.plot(range(1, len(average_history) + 1), average_history)
+plt.xlabel("Эпохи")
+plt.ylabel("Оценка МАЕ")
+plt.show()
